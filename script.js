e09fde97e6954a3a94a63756a24ffe9fbcca7faa
VagnerFM10 <vagnerm97@hotmail.com>
2024-03-20 16:59:18 -0300

    first commit

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function toggleMode() {
 
   //substituir a imagem
   if (html.classList.contains("light")) {
-    //se tiver light mode adionar a imagem light
-    img.setAttribute("src", "./avatar-light.png")
+    //se tiver light mode adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
